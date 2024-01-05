5f98cf2974b8b71bc3206e376162722d5c3c457b
Victor Marchenko <khizcom@yandex.ru>
2024-01-05 20:04:42 +0300

    finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCardFunc) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  cardDeleteButton.addEventListener('click', deleteCardFunc);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCardFunc(event) {
+  event.target.closest('.card').remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((el) => placesList.append(createCard(el, deleteCardFunc)));
